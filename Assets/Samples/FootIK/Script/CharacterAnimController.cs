b751a6ff4ff165ead6e52daa81d7a515da1f7115
heyChenfu <1576391213@qq.com>
2024-06-09 15:52:12 +0800

    足部IK权重逻辑

--- Assets/Samples/FootIK/Script/CharacterAnimController.cs
+++ Assets/Samples/FootIK/Script/CharacterAnimController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,13 +12,19 @@ public class CharacterAnimController : MonoBehaviour
     [SerializeField]
     public float WalkAnimAccelerate = 1;
     [SerializeField]
-    public float FootDistanceToGroud = 0;
+    public float FootDistanceToGroud = 0; //脚部贴地所需距离
     [SerializeField]
-    public float FootIKMaxDistance = 0;
+    public float FootIKMaxDistance = 0; //脚部IK检测最大距离
+    [SerializeField]
+    public float MaxFootDistance = 1; //脚部最大离开地面距离初始位置大小
 
     public LayerMask IkContainLayer;
+    public bool IsIK = true;
+    public bool IsMove = true;
 
     Animator animator;
+    Vector3 _footInitIKLeftFootPos = default;
+    Vector3 _footInitIKRightFootPos = default;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +36,7 @@ public class CharacterAnimController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         float horizontal = Input.GetAxis("Horizontal"); // 默认映射到左右箭头键和A、D键
         float vertical = Input.GetAxis("Vertical"); // 默认映射到上下箭头键和W、S键
 
@@ -44,6 +52,8 @@ public class CharacterAnimController : MonoBehaviour
             fWalkBlend = 0;
         }
         animator.SetFloat("WalkBlend", fWalkBlend);
+        if (!IsMove)
+            return;
         transform.Translate(direction * Time.deltaTime * WalkSpeed);
         transform.Rotate(0, horizontal * RotateSpeed * Time.deltaTime, 0);
 
@@ -51,10 +61,14 @@ public class CharacterAnimController : MonoBehaviour
 
     private void OnAnimatorIK(int layerIndex)
     {
-        animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
-        animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1);
-        animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
-        animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1);
+        if (!IsIK)
+            return;
+
+        Tuple<float, float> weightTupple = GetFootIKWeight();
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, weightTupple.Item1);
+        animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, weightTupple.Item1);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, weightTupple.Item2);
+        animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, weightTupple.Item2);
 
         RaycastHit hit;
         Ray rayLeftFoot = new Ray(animator.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
@@ -81,7 +95,29 @@ public class CharacterAnimController : MonoBehaviour
             }
         }
 
+    }
 
+    /// <summary>
+    /// 通过和初始距离大小计算当前IK权重
+    /// 即当脚一开始贴近地面, 而后移动动画的播放逐渐导致远离地面, 所以逐渐减小IK权重
+    /// </summary>
+    /// <returns></returns>
+    private Tuple<float, float> GetFootIKWeight()
+    {
+        if (_footInitIKLeftFootPos == default)
+            _footInitIKLeftFootPos = animator.transform.InverseTransformPoint(animator.GetIKPosition(AvatarIKGoal.LeftFoot));
+        if (_footInitIKRightFootPos == default)
+            _footInitIKRightFootPos = animator.transform.InverseTransformPoint(animator.GetIKPosition(AvatarIKGoal.RightFoot));
+        Vector3 currentLeftFootPosition = animator.transform.InverseTransformPoint(animator.GetIKPosition(AvatarIKGoal.LeftFoot));
+        Vector3 currentRightFootPosition = animator.transform.InverseTransformPoint(animator.GetIKPosition(AvatarIKGoal.RightFoot));
+        float disLeftSqr = Vector3.Distance(currentLeftFootPosition, _footInitIKLeftFootPos);
+        float disRightSqr = Vector3.Distance(currentRightFootPosition, _footInitIKRightFootPos);
+        float leftFootIKWeight = disLeftSqr / MaxFootDistance;
+        float rightFootIKWeight = disRightSqr / MaxFootDistance;
+        //Debug.Log($"leftFootIKWeight = {leftFootIKWeight}, rightFootIKWeight = {rightFootIKWeight}, disLeftSqr = {disLeftSqr}, disRightSqr = {disRightSqr}");
+        leftFootIKWeight = 1 - Mathf.Clamp01(leftFootIKWeight);
+        rightFootIKWeight = 1 - Mathf.Clamp01(rightFootIKWeight);
+        return Tuple.Create(leftFootIKWeight, rightFootIKWeight);
     }
 
 }
