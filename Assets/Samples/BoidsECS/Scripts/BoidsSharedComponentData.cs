95dcd94cf259ad26882b299ef15dfbb9f76663a6
owen <owen.chen@ingcreations.com>
2024-10-10 18:06:27 +0800

    添加Entities

---
+++ Assets/Samples/BoidsECS/Scripts/BoidsSharedComponentData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Unity.Entities;
+
+namespace BoidsECSSimulator 
+{
+    [Serializable]
+    public class BoidsSharedComponentData : ISharedComponentData
+    {
+        public float alignWeight;
+        public float cohesionWeight;
+        public float seperateWeight;
+        public float targetWeight;
+
+    }
+
+    public class BoidComponentData : IComponentData 
+    {
+        public float minSpeed;
+        public float maxSpeed;
+        public float boundsRadius;
+        public float collisionAvoidDst;
+        public int obstacleLayerMask;
+
+    }
+
+}
+
+
