323ed3f6250cc8e907daa0a3528fc9ee47662201
owen <owen.chen@ingcreations.com>
2024-10-22 18:03:30 +0800

    coding...

--- Assets/Samples/BoidsECS/Scripts/Authoring/BoidAuthoring.cs
+++ Assets/Samples/BoidsECS/Scripts/Authoring/BoidAuthoring.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace BoidsECSSimulator
@@ -8,6 +9,10 @@ namespace BoidsECSSimulator
     {
         public float minSpeed;
         public float maxSpeed;
+        public float perceptionRadius;
+        public float avoidanceRadius;
+        public float maxSteerForce;
+
         public float alignWeight;
         public float cohesionWeight;
         public float seperateWeight;
@@ -22,10 +27,13 @@ namespace BoidsECSSimulator
             public override void Bake(BoidAuthoring authoring)
             {
                 var entity = GetEntity(TransformUsageFlags.Renderable | TransformUsageFlags.WorldSpace);
-                AddSharedComponent(entity, new BoidComponentData
+                AddSharedComponent(entity, new BoidSharedComponentData
                 {
                     minSpeed = authoring.minSpeed,
                     maxSpeed = authoring.maxSpeed,
+                    perceptionRadius = authoring.perceptionRadius,
+                    avoidanceRadius = authoring.avoidanceRadius,
+                    maxSteerForce = authoring.maxSteerForce,
                     alignWeight = authoring.alignWeight,
                     cohesionWeight = authoring.cohesionWeight,
                     seperateWeight = authoring.seperateWeight,
@@ -35,20 +43,39 @@ namespace BoidsECSSimulator
                     collisionAvoidDst = authoring.collisionAvoidDst,
                     obstacleLayerMask = authoring.obstacleLayerMask,
                 });
+                AddComponent(entity, new BoidData());
+
             }
         }
     }
 
     [Serializable]
-    public struct BoidComponentData : ISharedComponentData
+    public struct BoidData : IComponentData 
+    {
+        public float3 Forward;
+        public float3 FlockHeading; //当前Boid感知到的所有邻居的方向总和
+        public float3 FlockCentre; //当前Boid感知到的所有邻居的位置总和
+        public float3 AvoidanceHeading; //当前Boid感知到的所有邻居的分离方向总和
+        public int NumFlockmates; //当前Boid感知到的邻居数量
+        public float3 Velocity;
+
+    }
+
+    [Serializable]
+    public struct BoidSharedComponentData : ISharedComponentData
     {
         public float minSpeed;
         public float maxSpeed;
+        public float perceptionRadius;//伙伴判定半径
+        public float avoidanceRadius;//规避判定半径
+        public float maxSteerForce;
+
         public float alignWeight;
         public float cohesionWeight;
         public float seperateWeight;
         public float targetWeight;
         public float avoidCollisionWeight;
+
         public float boundsRadius;
         public float collisionAvoidDst;
         public int obstacleLayerMask;
--- Assets/Samples/BoidsECS/Scripts/System/BoidSystem.cs
+++ Assets/Samples/BoidsECS/Scripts/System/BoidSystem.cs
@@ -3,10 +3,9 @@ using Unity.Burst;
 using Unity.Burst.Intrinsics;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
-using UnityEditor.Build.Content;
-using static UnityEngine.GraphicsBuffer;
 
 namespace BoidsECSSimulator
 {
@@ -15,7 +14,7 @@ namespace BoidsECSSimulator
     {
         public void OnCreate(ref SystemState state) 
         {
-            state.RequireForUpdate(state.GetEntityQuery(typeof(BoidComponentData)));
+            state.RequireForUpdate(state.GetEntityQuery(typeof(BoidSharedComponentData)));
         }
 
         public void OnDestroy(ref SystemState state) { }
@@ -23,14 +22,14 @@ namespace BoidsECSSimulator
         public void OnUpdate(ref SystemState state)
         {
             EntityQuery boidQuery = SystemAPI.QueryBuilder().
-                WithAll<BoidComponentData>().WithAllRW<LocalToWorld>().Build();
+                WithAll<BoidSharedComponentData>().WithAllRW<BoidData>().WithAllRW<LocalToWorld>().Build();
 
             var world = state.WorldUnmanaged;
-            state.EntityManager.GetAllUniqueSharedComponents(out NativeList<BoidComponentData> uniqueBoidTypes, world.UpdateAllocator.ToAllocator);
+            state.EntityManager.GetAllUniqueSharedComponents(out NativeList<BoidSharedComponentData> uniqueBoidTypes, world.UpdateAllocator.ToAllocator);
             float dt = math.min(0.05f, SystemAPI.Time.DeltaTime);
 
             //每次循环处理一组相同 Boid 配置的实体
-            foreach (BoidComponentData boidData in uniqueBoidTypes)
+            foreach (BoidSharedComponentData boidData in uniqueBoidTypes)
             {
                 boidQuery.AddSharedComponentFilter(boidData);
                 var boidCount = boidQuery.CalculateEntityCount();
@@ -39,14 +38,25 @@ namespace BoidsECSSimulator
                     boidQuery.ResetFilter();
                     continue;
                 }
-                NativeArray<float3> flockHeadingArr = CollectionHelper.CreateNativeArray<float3, RewindableAllocator>(
-                    boidCount, ref world.UpdateAllocator);
-                NativeArray<float3> flockCentreArr = CollectionHelper.CreateNativeArray<float3, RewindableAllocator>(
-                    boidCount, ref world.UpdateAllocator);
-                NativeArray<float3> avoidanceHeadingArr = CollectionHelper.CreateNativeArray<float3, RewindableAllocator>(
-                    boidCount, ref world.UpdateAllocator);
-                NativeArray<int> numFlockmatesArr = CollectionHelper.CreateNativeArray<int, RewindableAllocator>(
-                    boidCount, ref world.UpdateAllocator);
+                //NativeArray<float3> flockHeadingArr = CollectionHelper.CreateNativeArray<float3, RewindableAllocator>(
+                //    boidCount, ref world.UpdateAllocator);
+                //NativeArray<float3> flockCentreArr = CollectionHelper.CreateNativeArray<float3, RewindableAllocator>(
+                //    boidCount, ref world.UpdateAllocator);
+                //NativeArray<float3> avoidanceHeadingArr = CollectionHelper.CreateNativeArray<float3, RewindableAllocator>(
+                //    boidCount, ref world.UpdateAllocator);
+                //NativeArray<int> numFlockmatesArr = CollectionHelper.CreateNativeArray<int, RewindableAllocator>(
+                //    boidCount, ref world.UpdateAllocator);
+
+                var boidMainJob = new BoidMainCalJob() {
+                    BoidComponentDataHandle = SystemAPI.GetSharedComponentTypeHandle<BoidSharedComponentData>(),
+                    BoidDataHandle = SystemAPI.GetComponentTypeHandle<BoidData>(),
+                    LocalToWorldHandle = SystemAPI.GetComponentTypeHandle<LocalToWorld>(),
+                    //FlockHeadingArr = flockHeadingArr,
+                    //FlockCentreArr = flockCentreArr,
+                    //AvoidanceHeadingArr = avoidanceHeadingArr,
+                    //NumFlockmatesArr = numFlockmatesArr,
+                };
+                JobHandle boidMainJobHandle = boidMainJob.ScheduleParallel(boidQuery, state.Dependency);
 
                 boidQuery.ResetFilter();
             }
@@ -56,37 +66,89 @@ namespace BoidsECSSimulator
 
 
     [BurstCompile]
-    partial struct BoidMainCalJob : IJobChunk
+    unsafe struct BoidMainCalJob : IJobChunk
     {
-        [NativeDisableParallelForRestriction]
-        public EntityManager entityManager;
-
-        public SharedComponentTypeHandle<BoidComponentData> BoidComponentDataHandle;
+        public SharedComponentTypeHandle<BoidSharedComponentData> BoidComponentDataHandle;
+        public ComponentTypeHandle<BoidData> BoidDataHandle;
         [ReadOnly] public ComponentTypeHandle<LocalToWorld> LocalToWorldHandle;
-
-        public NativeArray<float3> FlockHeadingArr;
-        public NativeArray<float3> FlockCentreArr;
-        public NativeArray<float3> AvoidanceHeadingArr;
-        public NativeArray<int> NumFlockmatesArr;
 
         public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
         {
-            // 获取当前 chunk 中的 BoidComponentData 索引
-            int sharedComponentIndex = chunk.GetSharedComponentIndex(BoidComponentDataHandle);
-            // 通过 EntityManager 获取对应的 BoidSharedComponentData
-            BoidComponentData sharedData = entityManager.GetSharedComponentManaged<BoidComponentData>(sharedComponentIndex);
-
+            BoidSharedComponentData boidSharedData = chunk.GetSharedComponent(BoidComponentDataHandle);
+            BoidData* boidDataArr = chunk.GetComponentDataPtrRW(ref BoidDataHandle);
             NativeArray<LocalToWorld> localToWorldArr = chunk.GetNativeArray(ref LocalToWorldHandle);
             for (int i = 0; i < chunk.Count; i++)
             {
+                boidDataArr[i].NumFlockmates = 0;
+                boidDataArr[i].FlockHeading = float3.zero;
+                boidDataArr[i].FlockCentre = float3.zero;
+                boidDataArr[i].AvoidanceHeading = float3.zero;
                 for (int j = 0; j < chunk.Count; j++)
                 {
                     if (i == j)
                         continue;
-
+                    float3 offset = localToWorldArr[i].Position - localToWorldArr[j].Position;
+                    float sqrDst = offset.x * offset.x + offset.y * offset.y + offset.z * offset.z;
+                    if (sqrDst < boidSharedData.perceptionRadius * boidSharedData.perceptionRadius)
+                    {
+                        boidDataArr[i].NumFlockmates += 1;
+                        boidDataArr[i].FlockHeading+= localToWorldArr[j].Forward;
+                        boidDataArr[i].FlockCentre += localToWorldArr[j].Position;
+                        if (sqrDst < boidSharedData.avoidanceRadius * boidSharedData.avoidanceRadius)
+                            boidDataArr[i].AvoidanceHeading -= offset / sqrDst;
+                    }
                 }
+                UpdateBoid(i, boidSharedData, boidDataArr[i], localToWorldArr[i]);
             }
 
+        }
+
+        private void UpdateBoid(int index, BoidSharedComponentData boidSharedData, BoidData boidData, LocalToWorld localToWorld)
+        {
+            float3 acceleration = float3.zero;
+
+            if (boidData.NumFlockmates > 0)
+            {
+                //位置总和除以邻居数量得到邻居平均位置
+                float3 centreOfFlockmates = boidData.FlockCentre[index] / boidData.NumFlockmates;
+                //当前位置指向邻居平均位置向量
+                float3 offsetToFlockmatesCentre = centreOfFlockmates - localToWorld.Position;
+
+                //分别得到对齐,聚合,分离
+                var alignmentForce = SteerTowards(boidData.FlockHeading, boidSharedData, boidData) * boidSharedData.alignWeight;
+                var cohesionForce = SteerTowards(offsetToFlockmatesCentre, boidSharedData, boidData) * boidSharedData.cohesionWeight;
+                var seperationForce = SteerTowards(boidData.AvoidanceHeading, boidSharedData, boidData) * boidSharedData.seperateWeight;
+
+            }
+        }
+
+        private float3 SteerTowards(float3 vector, BoidSharedComponentData boidSharedData, BoidData boidData)
+        {
+            //目标方向和当前速度差值以得到所需的加速度向量
+            float3 v = Normalize(vector) * boidSharedData.maxSpeed - boidData.Velocity;
+            return ClampMagnitude(v, boidSharedData.maxSteerForce);
+        }
+
+        private float3 Normalize(float3 value)
+        {
+            float num = (float)math.sqrt(value.x * value.x + value.y * value.y + value.z * value.z);
+            if (num > 1E-05f)
+                return value / num;
+            return float3.zero;
+        }
+
+        public static float3 ClampMagnitude(float3 vector, float maxLength)
+        {
+            float num = vector.x * vector.x + vector.y * vector.y + vector.z * vector.z;
+            if (num > maxLength * maxLength)
+            {
+                float num2 = (float)math.sqrt(num);
+                float num3 = vector.x / num2;
+                float num4 = vector.y / num2;
+                float num5 = vector.z / num2;
+                return new float3(num3 * maxLength, num4 * maxLength, num5 * maxLength);
+            }
+            return vector;
         }
 
     }
